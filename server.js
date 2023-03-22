3a381ee06c75937b4fd048d3ed1e797e9c4891d7
Jared Collier <jared@Jareds-MBP.localdomain>
2023-03-22 15:27:23 -0600

    uses static middleware instead of traditional endpoints

--- server.js
+++ server.js
@@ -5,17 +5,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/styles.css')
-})
-
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname + '/public/main.js')
-})
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000, () => {
     console.log('app is up on 4000')
